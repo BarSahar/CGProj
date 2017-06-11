7553b50b1848d07262b466de271330b834d60605
Bar Sahar <bsahar1990@gmail.com>
2017-06-11 13:31:02 +0300

    asd

--- CGProj/cOGL.cs
+++ CGProj/cOGL.cs
@@ -20,6 +20,23 @@ namespace OpenGL
             p = pb;
             Width = p.Width;
             Height = p.Height;
+            /////TODO: delete this
+            //GL.glVertex3d(0, 0, 5);
+            //GL.glVertex3d(0, 10, 5);
+            //GL.glVertex3d(10, 10, 5);
+            /////
+            ground[0, 0] = 0f;
+            ground[0, 1] = 0f;
+            ground[0, 2] = 5.1f;
+
+            ground[1, 0] = 0f;
+            ground[1, 1] = 10f;
+            ground[1, 2] = 5.1f;
+
+            ground[2, 0] = 10f;
+            ground[2, 1] = 10f;
+            ground[2, 2] = 5.1f;
+
             InitializeGL();
             obj = GLU.gluNewQuadric(); //!!!
         }
@@ -131,13 +148,13 @@ namespace OpenGL
 
             //top - other texture
             GL.glTexCoord2f(0.0f, 0.0f);
-            GL.glVertex3d(0.5, 0.5, 5.1f);
+            GL.glVertex3d(0.5, 0.5, 5.01f);
             GL.glTexCoord2f(0.0f, 1.0f);
-            GL.glVertex3d(0.5, 9.5, 5.1f);
+            GL.glVertex3d(0.5, 9.5, 5.01f);
             GL.glTexCoord2f(1.0f, 1.0f);
-            GL.glVertex3d(9.5, 9.5, 5.1f);
+            GL.glVertex3d(9.5, 9.5, 5.01f);
             GL.glTexCoord2f(1.0f, 0.0f);
-            GL.glVertex3d(9.5, 0.5, 5.1f);
+            GL.glVertex3d(9.5, 0.5, 5.01f);
 
             GL.glEnd();
             GL.glBindTexture(GL.GL_TEXTURE_2D, Textures[0]);
@@ -264,10 +281,14 @@ namespace OpenGL
             GL.glDisable(GL.GL_TEXTURE_2D);
             GL.glPopMatrix();
         }
-        void DrawTheWomanInRed()
+        void DrawTheWomanInRed(bool shadow)
         {
             GL.glPushMatrix();// save starting position of drawing
-            GL.glColor3f(1.0f, 1.0f, 1.0f);
+            if (shadow ==true)
+                GL.glColor3f(0.0f, 0.0f, 0.0f);
+            else
+                GL.glColor3f(1.0f, 1.0f, 1.0f);
+
             //GL.glTranslatef(-5.0f, -5.0f, 5.2f);
             GL.glTranslatef(0f, 5f, 5.2f);
             GL.glRotated(intOptionC, 0, 0, 1); //rotating the dancer
@@ -557,7 +578,7 @@ namespace OpenGL
             GL.glTranslated(0f, 10f, -5f);   //moving into the mirror
             GL.glScalef(1, -1, 1);
             DrawChest();
-            DrawTheWomanInRed();
+            DrawTheWomanInRed(false);
             GL.glPopMatrix();
 
             GL.glDisable(GL.GL_STENCIL_TEST); //no need for the stencil anymore
@@ -579,12 +600,123 @@ namespace OpenGL
             //NOW draw the scene
             DrawChest();
             DrawChestLid();
-            DrawTheWomanInRed();
+            DrawTheWomanInRed(false);
+            // end of draw scene
+
+            GL.glPushMatrix();
+            //!!!!!!!!!!!!    		
+            MakeShadowMatrix(ground);
+            GL.glMultMatrixf(cubeXform);
+            //DrawObjects(true, 1);
+            DrawTheWomanInRed(true);
+            //!!!!!!!!!!!!!
+            GL.glPopMatrix();
+
             GL.glDisable(GL.GL_LIGHTING);
             GL.glFlush();
 
             WGL.wglSwapBuffers(m_uint_DC);
 
+        }
+        float[,] ground = new float[3, 3];//{ { 1, 1, -0.5 }, { 0, 1, -0.5 }, { 1, 0, -0.5 } };
+        
+        const int x = 0;
+        const int y = 1;
+        const int z = 2;
+        void calcNormal(float[,] v, float[] outp)
+        {
+            float[] v1 = new float[3];
+            float[] v2 = new float[3];
+
+            // Calculate two vectors from the three points
+            v1[x] = v[0, x] - v[1, x];
+            v1[y] = v[0, y] - v[1, y];
+            v1[z] = v[0, z] - v[1, z];
+
+            v2[x] = v[1, x] - v[2, x];
+            v2[y] = v[1, y] - v[2, y];
+            v2[z] = v[1, z] - v[2, z];
+
+            // Take the cross product of the two vectors to get
+            // the normal vector which will be stored in out
+            outp[x] = v1[y] * v2[z] - v1[z] * v2[y];
+            outp[y] = v1[z] * v2[x] - v1[x] * v2[z];
+            outp[z] = v1[x] * v2[y] - v1[y] * v2[x];
+
+            // Normalize the vector (shorten length to one)
+            ReduceToUnit(outp);
+        }
+        void ReduceToUnit(float[] vector)
+        {
+            float length;
+
+            // Calculate the length of the vector		
+            length = (float)Math.Sqrt((vector[0] * vector[0]) +
+                                (vector[1] * vector[1]) +
+                                (vector[2] * vector[2]));
+
+            // Keep the program from blowing up by providing an exceptable
+            // value for vectors that may calculated too close to zero.
+            if (length == 0.0f)
+                length = 1.0f;
+
+            // Dividing each element by the length will result in a
+            // unit normal vector.
+            vector[0] /= length;
+            vector[1] /= length;
+            vector[2] /= length;
+        }
+
+        float[] cubeXform = new float[16];
+        // Creates a shadow projection matrix out of the plane equation
+        // coefficients and the position of the light. The return value is stored
+        // in cubeXform[,]
+        void MakeShadowMatrix(float[,] points)
+        {
+            float[] planeCoeff = new float[4];
+            float dot;
+
+            // Find the plane equation coefficients
+            // Find the first three coefficients the same way we
+            // find a normal.
+            calcNormal(points, planeCoeff);
+
+            // Find the last coefficient by back substitutions
+            planeCoeff[3] = -(
+                (planeCoeff[0] * points[2, 0]) + (planeCoeff[1] * points[2, 1]) +
+                (planeCoeff[2] * points[2, 2]));
+
+
+            // Dot product of plane and light position
+            dot = planeCoeff[0] * pos[0] +
+                    planeCoeff[1] * pos[1] +
+                    planeCoeff[2] * pos[2] +
+                    planeCoeff[3];
+
+            // Now do the projection
+            // First column
+            cubeXform[0] = dot - pos[0] * planeCoeff[0];
+            cubeXform[4] = 0.0f - pos[0] * planeCoeff[1];
+            cubeXform[8] = 0.0f - pos[0] * planeCoeff[2];
+            cubeXform[12] = 0.0f - pos[0] * planeCoeff[3];
+
+            // Second column
+            cubeXform[1] = 0.0f - pos[1] * planeCoeff[0];
+            cubeXform[5] = dot - pos[1] * planeCoeff[1];
+            cubeXform[9] = 0.0f - pos[1] * planeCoeff[2];
+            cubeXform[13] = 0.0f - pos[1] * planeCoeff[3];
+
+            // Third Column
+            cubeXform[2] = 0.0f - pos[2] * planeCoeff[0];
+            cubeXform[6] = 0.0f - pos[2] * planeCoeff[1];
+            cubeXform[10] = dot - pos[2] * planeCoeff[2];
+            cubeXform[14] = 0.0f - pos[2] * planeCoeff[3];
+
+            // Fourth Column
+            cubeXform[3] = 0.0f - pos[3] * planeCoeff[0];
+            cubeXform[7] = 0.0f - pos[3] * planeCoeff[1];
+            cubeXform[11] = 0.0f - pos[3] * planeCoeff[2];
+            cubeXform[15] = dot - pos[3] * planeCoeff[3];
         }
 
         protected virtual void InitializeGL()
